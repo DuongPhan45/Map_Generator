35e3756b6e4374a112e28c73adf3719945e1acc6
DuongPhan45 <47710293+DuongPhan45@users.noreply.github.com>
2020-11-21 13:57:50 -0500

    Edge and Vertex Classes

---
+++ Edge.java
@@ -0,0 +1,37 @@
+package project3;
+
+//Name: Duong Phan
+//NetID: dphan7
+//Lab section: Gavett 244 TR 2-3.15pm
+public class Edge implements Comparable<Edge> {
+	public String ID;
+	public Vertex v1, v2;
+	public double len;
+
+	public Edge(Vertex v1, Vertex v2) {
+		this.v1 = v1;
+		this.v2 = v2;
+	}
+
+	public Edge(String ID, Vertex v1, Vertex v2) {
+		this(v1, v2);
+		this.ID = ID;
+	}
+
+	public Edge(Vertex v1, Vertex v2, double len) {
+		this(v1, v2);
+		this.len = len;
+	}
+
+	@Override
+	public int compareTo(Edge e) {
+		if (len > e.len)
+			return 1;
+		else if (len < e.len)
+			return -1;
+		else {
+			return 0;
+		}
+
+	}
+}
---
+++ Vertex.java
@@ -0,0 +1,45 @@
+package project3;
+
+//Name: Duong Phan
+//NetID: dphan7
+//Lab section: Gavett 244 TR 2-3.15pm
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+public class Vertex implements Comparable<Vertex> {
+	public String ID;
+	public int indexLabel;
+	public double x; // longitude
+	public double y;// latitude
+	double distance;
+	boolean settled;
+	int rank; // Number of vertices have this as head
+	Vertex lastVertex;// the last vertex that lead to this in finding shortest path
+	Vertex head; // the head vertex that represents a set in Union Find
+	public HashMap<String, Vertex> neighbor;// Store all the neighbors of the vertex
+
+	public Vertex(double x, double y) {
+		this.x = x;
+		this.y = y;
+		neighbor = new HashMap<String, Vertex>();
+		head = this; // Initiate the Vertex itself as its head
+		rank = 0;// Initial rank
+
+	}
+
+	public Vertex(String ID, double x, double y) {
+		this(x, y);
+		this.ID = ID;
+	}
+
+	@Override
+	public int compareTo(Vertex v) {
+		if (distance < v.distance)
+			return -1;
+		else if (distance > v.distance)
+			return 1;
+		return 0;
+	}
+
+}
